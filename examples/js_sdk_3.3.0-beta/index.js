bbe4ed0520a1b0033a66523c2d4087b9815741be
Matt Carroll <matt.carroll@optimizely.com>
2019-08-26 15:25:26 -0700

    Add Node example

---
+++ examples/js_sdk_3.3.0-beta/index.js
@@ -0,0 +1,33 @@
+const optimizelySdk = require('@optimizely/optimizely-sdk');
+
+async function main() {
+  optimizelySdk.setLogLevel('debug');
+  optimizelySdk.setLogger(optimizelySdk.logging.createLogger());
+
+  const optimizelyInstance = optimizelySdk.createInstance({
+    sdkKey: '9LCprAQyd1bs1BBXZ3nVji',
+    eventBatchSize: 20,
+    eventFlushInterval: 5000, // ms
+  });
+
+  const result = await optimizelyInstance.onReady({ timeout: 5000 });
+  console.log(`Optimizely ready: ${JSON.stringify(result)}`);
+  const user = `user${Date.now()}`;
+
+  // Generate impression events:
+
+  // From A/B test
+  // optimizelyInstance.activate('<PASTE YOUR EXPERIMENT KEY HERE>', user);
+
+  // From Feature test
+  // optimizelyInstance.isFeatureEnabled('<PASTE YOUR FEATURE KEY HERE>', user);
+
+
+  // Generate conversion events
+  // optimizelyInstance.track('<PASTE YOUR EVENT KEY HERE>', user);
+
+  // Close the instance, which immediately flushes the current batch of events
+  // optimizelyInstance.close();
+}
+
+main();
